9355e17fd20b9939804d32299be3c963e9b6ef29
Cheikh Mbacke <mbackecheikhouna7@gmail.com>
2023-03-15 19:48:38 +0100

    import des données du json + mise en page de la page d'acceuil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,51 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        article.classList.add('portrait');
+
+        const lien = document.createElement( 'a' );
+        lien.setAttribute("href", `photographer.html?id=${id}`);
+
+        const div = document.createElement('div');
+        div.classList.add('contentImg');
+
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", name);
+        img.setAttribute("id", id);
+        img.classList.add("img-photographer");
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
+
+        const pLocalisation = document.createElement( 'p' );
+        pLocalisation.textContent = city + ", " + country;
+        pLocalisation.classList.add('location');
+
+        const pTagline = document.createElement( 'p' );
+        pTagline.textContent = tagline;
+        pTagline.classList.add('tagline');
+
+        const pPrice = document.createElement( 'p' );
+        pPrice.textContent = price + "€/jour";
+        pPrice.classList.add('price');
+
+        article.appendChild(lien);
+        lien.appendChild(div);
+        div.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(pLocalisation);
+        article.appendChild(pTagline);
+        article.appendChild(pPrice);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    function navigateToPhotographerPage() {
+        const params = new URLSearchParams(window.location.search);
+        const photographerId = params.get('id');
+    }
+    return { name, picture, img:name, img:id, pLocalisation:city, pTagline:tagline, pPrice:price, getUserCardDOM, navigateToPhotographerPage }
 }
